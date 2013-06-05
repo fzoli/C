7013b4e5cd7a05576cc974bcb239f30c693b4e25
fzoli <f.zoli@mailbox.hu>
2013-06-05 16:12:32 +0200

    modified:   QtRadioInfo2/build/Release/GNU-Linux-x86/qrc_images.o 	modified:   QtRadioInfo2/dist/Release/GNU-Linux-x86/QtRadioInfo2

--- QtRadioInfo2/moc_RadioInfoForm.cpp
+++ QtRadioInfo2/moc_RadioInfoForm.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'RadioInfoForm.h'
 **
-** Created: Wed May 15 22:09:47 2013
+** Created: Wed Jun 5 16:12:25 2013
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.3)
 **
 ** WARNING! All changes made in this file will be lost!
--- QtRadioInfo2/ui_RadioInfoForm.h
+++ QtRadioInfo2/ui_RadioInfoForm.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'RadioInfoForm.ui'
 **
-** Created: Wed May 15 22:09:44 2013
+** Created: Wed Jun 5 16:12:19 2013
 **      by: Qt User Interface Compiler version 4.6.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
